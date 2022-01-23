58f64f9b3713a1edc82d344c2b59fbe846cecc06
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2022-01-23 18:09:38 +0100

    Remove duplicate code

--- Frends.Kungsbacka.Json/MapTask.cs
+++ Frends.Kungsbacka.Json/MapTask.cs
@@ -13,47 +13,6 @@ namespace Frends.Kungsbacka.Json
     /// </summary>
     public static class MapTask
     {
-
-        /// <summary>
-        /// Convert a json string to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertJsonStringToJToken(string json)
-        {
-            return JToken.Parse(json);
-        }
-
-        /// <summary>
-        /// Convert XML string to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertXmlStringToJToken(string xml)
-        {
-            var doc = new XmlDocument();
-            doc.LoadXml(xml);
-            var jsonString = JsonConvert.SerializeXmlNode(doc);
-            return JToken.Parse(jsonString);
-
-        }
-
-        /// <summary>
-        /// Convert XML byte array to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertXmlBytesToJToken(byte[] xml)
-        {
-            using (var stream = new MemoryStream(xml))
-            {
-                using (var xmlreader = new XmlTextReader(stream))
-                {
-                    var doc = new XmlDocument();
-                    doc.Load(xmlreader);
-                    var jsonString = JsonConvert.SerializeXmlNode(doc);
-                    return JToken.Parse(jsonString);
-                }
-            }
-        }
-
         /// <summary>
         /// Maps properties from one JObject to another. A default value can be specified if the property
         /// is not found in the source object. Optionally simple transforms can be applied to the value.
