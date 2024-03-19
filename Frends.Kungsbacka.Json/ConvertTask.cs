24ea37b2c6f59b2bff3c083f4cdf6bb513bb55a1
patriknilssonk <97089559+patriknilssonk@users.noreply.github.com>
2024-03-19 12:33:29 +0100

    Add methods using XmlReader

--- Frends.Kungsbacka.Json/ConvertTask.cs
+++ Frends.Kungsbacka.Json/ConvertTask.cs
@@ -1,53 +1,192 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Runtime;
 using System.Xml;
 
 namespace Frends.Kungsbacka.Json
 {
-    /// <summary>
-    /// JsonSchema Tasks
-    /// </summary>
-    public static partial class JsonTasks
-    {
-        /// <summary>
-        /// Convert a json string to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertJsonStringToJToken(string json)
-        {
-            return JToken.Parse(json);
-        }
+	/// <summary>
+	/// JsonSchema Tasks
+	/// </summary>
+	public static partial class JsonTasks
+	{
+		/// <summary>
+		/// Convert a json string to JToken
+		/// </summary>
+		/// <returns>JToken</returns>
+		public static JToken ConvertJsonStringToJToken(string json)
+		{
+			return JToken.Parse(json);
+		}
 
-        /// <summary>
-        /// Convert XML string to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertXmlStringToJToken(string xml)
-        {
-            var doc = new XmlDocument();
-            doc.LoadXml(xml);
-            var jsonString = JsonConvert.SerializeXmlNode(doc);
-            return JToken.Parse(jsonString);
+		/// <summary>
+		/// Convert XML string to JToken
+		/// </summary>
+		/// <returns>JToken</returns>
+		public static JToken ConvertXmlStringToJToken(string xml)
+		{
+			var doc = new XmlDocument();
+			doc.LoadXml(xml);
+			var jsonString = JsonConvert.SerializeXmlNode(doc);
+			return JToken.Parse(jsonString);
+		}
+		/// <summary>
+		/// Convert XML byte array to JToken
+		/// </summary>
+		/// <returns>JToken</returns>
+		public static JToken ConvertXmlBytesToJToken(byte[] xml)
+		{
+			using (var stream = new MemoryStream(xml))
+			{
+				using (var xmlreader = new XmlTextReader(stream))
+				{
+					var doc = new XmlDocument();
+					doc.Load(xmlreader);
+					var jsonString = JsonConvert.SerializeXmlNode(doc);
+					return JToken.Parse(jsonString);
+				}
+			}
+		}
 
-        }
+		/// <summary>
+		/// Convert XML string to JToken using xmlreader and using specific elementnames. 
+		/// Using singleElement will return the first object.
+		/// Using asJToken will all objects in one single jToken.
+		/// </summary>
+		/// <returns>JToken</returns>
+		public static JToken ConvertXmlStringToJTokenXmlReader([PropertyTab] XmlReaderInputString input, [PropertyTab] XmlReaderInputSettings settings)
+		{
+			IEnumerable<XmlNode> result;
 
-        /// <summary>
-        /// Convert XML byte array to JToken
-        /// </summary>
-        /// <returns>JToken</returns>
-        public static object ConvertXmlBytesToJToken(byte[] xml)
-        {
-            using (var stream = new MemoryStream(xml))
-            {
-                using (var xmlreader = new XmlTextReader(stream))
-                {
-                    var doc = new XmlDocument();
-                    doc.Load(xmlreader);
-                    var jsonString = JsonConvert.SerializeXmlNode(doc);
-                    return JToken.Parse(jsonString);
-                }
-            }
-        }
-    }
+			using (var stringReader = new StringReader(input.Xml))
+			{
+				using (var xmlReader = XmlReader.Create(stringReader, XmlReaderSettings(settings.SingleElement, settings.SelectRootElement)))
+				{
+					result = ReadXmlNodes(xmlReader, settings.Elements, settings.SelectRootElement, settings.SingleElement);
+				}
+			}
+
+			if(settings.AsSingleJtoken)
+			{
+				return XmlNodeListToJTokenOnlyValues(result);
+			}
+
+			if (!result.Any()) return new JArray();
+
+			return settings.SingleElement || settings.SelectRootElement ? JToken.FromObject(result.FirstOrDefault()) : JToken.FromObject(result);
+		}
+
+		/// <summary>
+		/// Convert XML byte array to JToken using xmlreader and using specific elementnames
+		/// </summary>
+		/// <returns>JToken</returns>
+		public static JToken ConvertXmlBytesToJTokenXmlReader([PropertyTab] XmlReaderInputBytes input, [PropertyTab] XmlReaderInputSettings settings)
+		{
+			IEnumerable<XmlNode> result;
+
+			using (var stream = new MemoryStream(input.Xml))
+			{
+				using (var xmlReader = XmlReader.Create(stream, XmlReaderSettings(settings.SingleElement, settings.SelectRootElement)))
+				{
+					result = ReadXmlNodes(xmlReader, settings.Elements, settings.SelectRootElement, settings.SingleElement);
+				}
+			}
+
+			if (settings.AsSingleJtoken)
+			{
+				return XmlNodeListToJTokenOnlyValues(result);
+			}
+
+			if(!result.Any()) return new JArray();
+
+			return settings.SingleElement || settings.SelectRootElement ? JToken.FromObject(result.FirstOrDefault()) : JToken.FromObject(result);
+		}
+
+		private static IEnumerable<XmlNode> ReadXmlNodes(XmlReader xmlReader, List<string> elements, bool selectRootElement, bool singleElement)
+		{
+			var doc = new XmlDocument();
+			var nodeList = new List<XmlNode>();
+
+			while (xmlReader.Read())
+			{
+				if (xmlReader.NodeType == XmlNodeType.Element)
+				{
+					if(selectRootElement)
+					{
+						nodeList.Add(doc.ReadNode(xmlReader));
+
+						break;
+					}
+
+					if (elements.Contains(xmlReader.Name, StringComparer.OrdinalIgnoreCase))
+					{
+						nodeList.Add(doc.ReadNode(xmlReader));
+						if (singleElement) break;
+					}
+				}
+			}
+
+			if(!singleElement || selectRootElement && nodeList.Any())
+			{
+				RemoveWhitespaceNodes(nodeList);
+			}
+
+			return nodeList;
+		}
+
+		private static void RemoveWhitespaceNodes(IEnumerable<XmlNode> nodeList)
+		{
+			foreach (XmlNode node in nodeList)
+			{
+				for (int i = 0; i < node.ChildNodes.Count; i++)
+				{
+					if (node.ChildNodes[i].NodeType == XmlNodeType.Whitespace)
+					{
+						node.RemoveChild(node.ChildNodes[i]);
+					}
+				}
+			}
+		}
+
+		private static readonly string[] AttributesToIgnore = { "xmlns" };	
+
+		private static JToken XmlNodeListToJTokenOnlyValues(IEnumerable<XmlNode> xmlNodes)
+		{
+			var result = new JObject();
+			
+			foreach (XmlNode node in xmlNodes)
+			{
+				if (!result.ContainsKey(node.Name))
+				{
+					result.Add(node.Name, node.InnerXml);
+
+					if (node.Attributes.Count > 0)
+					{
+                        foreach (XmlAttribute nodeAttribute in node.Attributes)
+                        {
+							if(!AttributesToIgnore.Contains($"@{nodeAttribute.Name}") && !result.ContainsKey($"@{nodeAttribute.Name}"))
+							{
+								result.Add($"@{nodeAttribute.Name}", nodeAttribute.InnerText);
+							}
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static XmlReaderSettings XmlReaderSettings(bool singleElement, bool selectRootElement)
+		{
+			return new XmlReaderSettings
+			{
+				IgnoreWhitespace = singleElement || selectRootElement
+			};
+		}
+	}
 }
--- Frends.Kungsbacka.Json/MapDefinition.cs
+++ Frends.Kungsbacka.Json/MapDefinition.cs
@@ -1,83 +1,81 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Frends.Kungsbacka.Json
 {
-    /// <summary>
-    /// Required parameters for Map task
-    /// </summary>
-    public class MapInput
-    {
-        /// <summary>
-        /// Source JObject (mandatory)
-        /// </summary>
-        [DisplayFormat(DataFormatString = "Expression")]
-        public Newtonsoft.Json.Linq.JObject SourceObject { get; set; }
+	/// <summary>
+	/// Required parameters for Map task
+	/// </summary>
+	public class MapInput
+	{
+		/// <summary>
+		/// Source JObject (mandatory)
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Expression")]
+		public Newtonsoft.Json.Linq.JObject SourceObject { get; set; }
 
-        /// <summary>
-        /// Destination JObject. If destination object is not supplied (null),
-        /// a new JObject is created.
-        /// </summary>
-        [DisplayFormat(DataFormatString = "Expression")]
-        public Newtonsoft.Json.Linq.JObject DestinationObject { get; set; }
+		/// <summary>
+		/// Destination JObject. If destination object is not supplied (null),
+		/// a new JObject is created.
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Expression")]
+		public Newtonsoft.Json.Linq.JObject DestinationObject { get; set; }
 
-        /// <summary>
-        /// JSON that describes the mappings. Array of:
-        /// {
-        ///     "from": "from name",
-        ///     "to": "to name",
-        ///     "def": "Optional default",
-        ///     "trans": ["Array of transformations"]
-        /// }
-        /// 
-        /// Def can be used to supply a default value if the source does not
-        /// exist. Trans applies transformations to the source token before it
-        /// is added to the destination object.
-        /// 
-        /// From property supports using Json.NETs SelectToken query syntax (
-        /// https://www.newtonsoft.com/json/help/html/SelectToken.htm). 
-        /// </summary>
-        [DisplayFormat(DataFormatString = "Json")]
-        [DefaultValue(@"[
+		/// <summary>
+		/// JSON that describes the mappings. Array of:
+		/// {
+		///     "from": "from name",
+		///     "to": "to name",
+		///     "def": "Optional default",
+		///     "trans": ["Array of transformations"]
+		/// }
+		/// 
+		/// Def can be used to supply a default value if the source does not
+		/// exist. Trans applies transformations to the source token before it
+		/// is added to the destination object.
+		/// 
+		/// From property supports using Json.NETs SelectToken query syntax (
+		/// https://www.newtonsoft.com/json/help/html/SelectToken.htm). 
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Json")]
+		[DefaultValue(@"[
     {""from"": ""Firstname"", ""to"": ""first""},
     {""from"": ""Lastname"", ""to"": ""last""},
     {""from"": ""NewUser"", ""to"": ""new"", ""def"": false},
     {""from"": ""Description"", ""to"": ""desc"", ""trans"": [""Trim""]}
 ]")]
-        public string Map { get; set; }
-    }
+		public string Map { get; set; }
+	}
 
-    /// <summary>
-    /// Optional parameters for Map task
-    /// </summary>
-    public class MapOptions
-    {
-        /// <summary>
-        /// If a source object contains a #cdata-section, get the
-        /// value inside instead. #cdata-sections is a common
-        /// result of XML to JSON conversions.
-        /// </summary>
-        [DefaultValue(false)]
-        public bool UnpackCdataSection { get; set; }
+	/// <summary>
+	/// Optional parameters for Map task
+	/// </summary>
+	public class MapOptions
+	{
+		/// <summary>
+		/// If a source object contains a #cdata-section, get the
+		/// value inside instead. #cdata-sections is a common
+		/// result of XML to JSON conversions.
+		/// </summary>
+		[DefaultValue(false)]
+		public bool UnpackCdataSection { get; set; }
 
-        /// <summary>
-        /// Array of custom transformations
-        /// </summary>
-        public MapTransformation[] Tranformations { get; set; }
-    }
+		/// <summary>
+		/// Array of custom transformations
+		/// </summary>
+		public MapTransformation[] Tranformations { get; set; }
+	}
 
-    /// <summary>
-    /// Custom transformation
-    /// </summary>
-    public class MapTransformation
-    {
-        /// <summary>
-        /// Transformation name
-        /// </summary>
-        public string TransformationName { get; set; }
-
+	/// <summary>
+	/// Custom transformation
+	/// </summary>
+	public class MapTransformation
+	{
+		/// <summary>
+		/// Transformation name
+		/// </summary>
+		public string TransformationName { get; set; }
         /// <summary>
         /// Transformation function. Must be a Func&lt;JToken,JToken&gt;
         /// Example function that takes the first element in an array.
@@ -97,4 +95,78 @@ namespace Frends.Kungsbacka.Json
         /// </summary>
         public dynamic TransformationFunction { get; set; }
     }
+	/// <summary>
+	/// Inputparameter for converting string to jtoken
+	/// </summary>
+	public class XmlReaderInputString
+	{
+		/// <summary>
+		/// Source xml as bytes 
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Expression")]
+		public string Xml { get; set; }
+	}
+	/// <summary>
+	/// Inputparameter for converting bytes to jtoken
+	/// </summary>
+	public class XmlReaderInputBytes
+	{
+		/// <summary>
+		/// Source xml as bytes 
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Expression")]
+		public byte[] Xml { get; set; }
+	}
+	/// <summary>
+	/// Settings for converting xml to JToken using xmlreader.
+	/// </summary>
+	public class XmlReaderInputSettings
+	{
+		/// <summary>
+		/// Will return the root element of the xml. Will ignore Elements-parameter
+		/// </summary>
+		[DefaultValue(true)]
+		public bool SelectRootElement { get; set; }
+		/// <summary>
+		/// List of elements used to get specific values from xml
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Expression")]
+		[DefaultValue(@"new List<string> { """" }")]
+		public List<string> Elements { get; set; }
+		/// <summary>
+		/// Used when the expected result is only one object, when the xmlnode is found the rest will be skipped.
+		/// This can be used for getting a specific named element anywhere in the xml. 
+		/// Example:
+		///  <root>
+		///   <animal>
+		///    <name>Tom</name>
+		///    <type>Cat</type>
+		///  </animal>
+		/// </root>
+		/// Combining SingleElement with "animal" in Elements will return the "animal" together with the children "name" and "type".
+		/// Combining SingleElement with "type" in Elements will return the "type" node.
+		/// </summary>
+		[DefaultValue(false)]
+		public bool SingleElement { get; set; }
+		/// <summary>
+		/// Using this will return all the values and attributes on the found nodes as a flat JToken. All children will be ignored.
+		/// Used for only getting specific values from an xml. Should probably no be used on a node with children.
+		/// Example:
+		/// <root>
+		///  <animal>
+		///    <name>Tom</name>
+		///    <type id='1'>Cat</type>
+		///  </animal>
+		/// </root>
+		/// Combining ResultAsJTokenOnlyValues with "type, name" in Elements will return a JToken with the following data:
+		/// {
+		///		{ "name": "Tom " },
+		///		{ "type": "Cat " },
+		///		{ "id": "1 " },
+		/// }
+		/// </summary>
+		[DefaultValue(false)] 
+		public bool AsSingleJtoken { get; set; }
+
+	}
 }
